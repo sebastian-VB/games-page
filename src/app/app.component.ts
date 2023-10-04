b1c856f402c4b4596ca0ffe36778c30cb779afa2
sebastian-VB <sebas99peru@gmail.com>
2023-10-04 13:15:56 -0500

    created header - only design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
