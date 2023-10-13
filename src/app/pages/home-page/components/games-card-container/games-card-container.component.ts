e64b7fb60c4180857f1affbe7b730ba207d64a7e
sebastian-VB <sebas99peru@gmail.com>
2023-10-13 11:19:07 -0500

    show games data in cards

--- src/app/pages/home-page/components/games-card-container/games-card-container.component.ts
+++ src/app/pages/home-page/components/games-card-container/games-card-container.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from 'src/app/global/interfaces/game.interface';
 
 @Component({
   selector: 'app-games-card-container',
@@ -9,5 +10,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./games-card-container.component.scss']
 })
 export class GamesCardContainerComponent {
+
+  @Input() gameInfo!: Game;
 
 }
